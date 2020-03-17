ea1e6ae397a3639bb960749f0036c86e5d6c9d9f
pedgomgal1 <pgomez-ibis@us.es>
2020-03-17 12:47:18 +0100

    common segmentaiton postProcessing - LimeSeg and PlantSeg output

---
+++ src/segmentationPostProcessing.m
@@ -0,0 +1,68 @@
+function [outputArg1,outputArg2] = segmentationPostProcessing(labelledImage,lumenImage,apicalLayer,basalLayer,outputDir,resizeImg,tipValue,glandOrientation,colours)
+    %%Once lumen and cells have been adquired, this function provide a
+    %%modification by means of window() 
+
+    outsideGland = labelledImage == 0 & imdilate(lumenImage, strel('sphere', 1)) == 0;
+
+    
+    
+    setappdata(0,'outputDir', outputDir);
+    setappdata(0,'labelledImage',labelledImage);
+    setappdata(0,'lumenImage', lumenImage);
+    setappdata(0,'resizeImg',resizeImg);
+    setappdata(0,'tipValue', tipValue);
+    setappdata(0, 'glandOrientation', glandOrientation);
+    setappdata(0, 'canModifyOutsideGland', 0);
+    setappdata(0, 'hideLumen',0);
+    setappdata(0, 'canModifyInsideLumen',0);
+    setappdata(0, 'colours', colours);
+
+
+    if exist(fullfile(outputDir, 'Results', 'valid_cells.mat'), 'file')
+        load(fullfile(outputDir, 'Results', 'valid_cells.mat'))
+    else
+        [noValidCells] = insertNoValidCells();
+        validCells = setdiff(1:max(labelledImage(:)), noValidCells);
+        if noValidCells == -1
+            noValidCells = [];
+        end 
+        save(fullfile(outputDir, 'Results', 'valid_cells.mat'), 'noValidCells', 'validCells')
+    end
+    [answer, apical3dInfo, notFoundCellsApical, basal3dInfo, notFoundCellsBasal] = calculateMissingCells(addTipsImg3D(tipValue,labelledImage), addTipsImg3D(tipValue,lumenImage), addTipsImg3D(tipValue,apicalLayer),addTipsImg3D(tipValue,basalLayer), colours, noValidCells);
+
+    %% Insert no valid cells
+    while isequal(answer, 'Yes')
+        h = window();
+        waitfor(h);
+
+        savingResults = saveResults();
+
+        if isequal(savingResults, 'Yes')
+            labelledImage = getappdata(0, 'labelledImageTemp');
+            lumenImage = getappdata(0, 'lumenImage');
+            close all
+            [labelledImage, basalLayer, apicalLayer] = postprocessGland(labelledImage,labelledImage==0, lumenImage, outputDir, colours, tipValue);
+           
+            %% Save apical and basal 3d information
+            save(fullfile(outputDir, 'Results', '3d_layers_info.mat'), 'labelledImage', 'basalLayer', 'apicalLayer', 'apical3dInfo', 'basal3dInfo', 'colours', 'lumenImage','glandOrientation', '-v7.3')
+    
+            [answer, apical3dInfo, notFoundCellsApical, basal3dInfo, notFoundCellsBasal] = calculateMissingCells(addTipsImg3D(tipValue,labelledImage), addTipsImg3D(tipValue,lumenImage), addTipsImg3D(tipValue,apicalLayer),addTipsImg3D(tipValue,basalLayer), colours, noValidCells);
+        else
+            [answer] = isEverythingCorrect();
+        end
+        setappdata(0,'labelledImage',labelledImage);
+    end
+
+    %% Save apical and basal 3d information
+    save(fullfile(outputDir, 'Results', '3d_layers_info.mat'), 'labelledImage', 'basalLayer', 'apicalLayer', 'apical3dInfo', 'basal3dInfo', 'colours', 'lumenImage','glandOrientation', '-v7.3')
+
+    %% Export to excel cellular features
+    cellularFeatures = calculate_CellularFeatures(apical3dInfo,basal3dInfo,apicalLayer,basalLayer,addTipsImg3D(tipValue,labelledImage),noValidCells,validCells,outputDir);
+    
+    save(fullfile(outputDir, 'Results', 'cellularFeaturesExcel.mat'), 'cellularFeatures'); 
+
+
+
+
+end
+
